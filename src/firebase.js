620a9efc46b3f70ca714c16ebd9c8918e706e93e
omkar patil <omkarpatil3903@gmail.com>
2026-01-02 12:06:39 +0530

    feat: Add Firebase initialization and configuration.

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,22 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth'; // Import auth
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7EqMUFLJXvUVGUr-MlEJMrjSqxdDUnOU",
-  authDomain: "scroller-4d10f.firebaseapp.com",
-  databaseURL: "https://scroller-4d10f-default-rtdb.firebaseio.com",
-  projectId: "scroller-4d10f",
-  storageBucket: "scroller-4d10f.appspot.com",
-  messagingSenderId: "1053362115345",
-  appId: "1:1053362115345:web:1e42a1c584dae0765a32b0",
-  measurementId: "G-7Y65NLWMKL"
+  // apiKey: "AIzaSyD7EqMUFLJXvUVGUr-MlEJMrjSqxdDUnOU",
+  // authDomain: "scroller-4d10f.firebaseapp.com",
+  // databaseURL: "https://scroller-4d10f-default-rtdb.firebaseio.com",
+  // projectId: "scroller-4d10f",
+  // storageBucket: "scroller-4d10f.appspot.com",
+  // messagingSenderId: "1053362115345",
+  // appId: "1:1053362115345:web:1e42a1c584dae0765a32b0",
+  // measurementId: "G-7Y65NLWMKL"
+  apiKey: "AIzaSyAA_wVVtYrTTn_-vzslIQ-6L3Bc4Vh8TXU",
+  authDomain: "smartpracharakpro.firebaseapp.com",
+  databaseURL: "https://smartpracharakpro-default-rtdb.firebaseio.com",
+  projectId: "smartpracharakpro",
+  storageBucket: "smartpracharakpro.firebasestorage.app",
+  messagingSenderId: "129702576828",
+  appId: "1:129702576828:web:c94e054a2da7d4e9e306b9",
+  measurementId: "G-W478ME9D3W"
 };
 
 // Initialize Firebase App
